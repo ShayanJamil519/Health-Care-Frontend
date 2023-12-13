05f436d7ed3ade5251d9e24ec73c5b222c238142
ShayanJamil519 <shayanjamil500@gmail.com>
2023-12-13 07:13:19 +0500

    Footer Added

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className=" w-full md:w-[90%] mx-auto py-5 flex md:flex-row flex-col justify-center items-center gap-1 md:gap-8 font-urbanist text-[#181C23] border-t-[1px] border-[#8D8E9C]">
+      <p className="font-normal text-sm sm:text-base">
+        © 2023 . All rights reserved.
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
